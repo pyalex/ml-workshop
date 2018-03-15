4a682f5223a18ef7522c3004a74e31c3ce551246
Oleksii Moskalenko <oleksiimo@wix.com>
2018-03-15 12:47:19 +0200

    watch

--- auth/watch.py
+++ auth/watch.py
@@ -2,9 +2,19 @@ import requests
 import os
 import json
 
-token = "ya29.GltyBVvfb7ORaXIVAKwyeaCgLnpVEYEbUV0b7JZPHnoiGnIDOFtqC7evfo2mmcaReTYGnT83-mUCJwufJt-GNp5iltyS9HwLBYer4QvNkPzomiONiK8mQlm1d1_e"
-token = "ya29.Glx7BVnKNCRAySXwNByZhaiu-WCTiACsKaD44Wkt82H84iqM4Tj5w4ath12Km2LMpcU2OnYs1zDTaE3TNNYZ64Eh9IwWb7AgNl0NN6c0dqcgj5jbLFcEY-ARTfUhyg"
-topic = 'projects/wix-wision/topics/oleksii-gmail'
 url = "https://www.googleapis.com/gmail/v1/users/me/watch"
 
-requests.post(url, params=dict(access_token=token), data=json.dumps(dict(topicName=topic, labelIds=["INBOX"])), headers={'Content-type': 'application/json'})
+
+def watch(token, topic):
+    requests.post(url, params=dict(access_token=token),
+                  data=json.dumps(dict(topicName=topic, labelIds=["INBOX"])),
+                  headers={'Content-type': 'application/json'})
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--token", type=str, default="", help="token")
+    parser.add_argument("--topic", type=str, default="projects/wix-wision/topics/oleksii-gmail", help="topic")
+    args = parser.parse_args()
+
+    watch(args.token, args.topic)
