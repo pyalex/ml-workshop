b118d1494d4012b7896d41622fac8d341425416b
Oleksii Moskalenko <oleksiimo@wix.com>
2018-03-17 15:29:12 +0200

    Sample2

---
+++ processor/src/main/scala/Sample2.scala
@@ -0,0 +1,49 @@
+import javax.mail.internet.MimeMessage
+
+import com.spotify.scio.ContextAndArgs
+import org.apache.beam.sdk.io.TextIO
+import utils.Data.Email
+import utils.Parser.parseDate
+import utils.{MailUtils, Parser}
+
+import scala.util.Try
+
+
+
+
+
+object Sample2 {
+
+  def main(cmdLineArgs: Array[String]): Unit = {
+    val (sc, args) = ContextAndArgs(cmdLineArgs)
+    val myEmail = "moskalenko.alexey@gmail.com"
+    implicit val aaa: (String => MimeMessage) = MailUtils.parseEmail(encoded = false)(_)
+
+    val reader = TextIO.read()
+      .from(args("input"))
+      .withDelimiter("\nFrom ".getBytes)
+
+    val raw = sc.wrap(sc.pipeline.apply(reader))
+
+    val inbounds = raw.filter( x => {
+      val email = Parser.parseEmail(x).get
+      email.from != myEmail
+    })
+
+    val outbounds = raw.filter( x => {
+      val email = Parser.parseEmail(x).get
+      email.from == myEmail
+    })
+
+
+    val output = inbounds.sample(withReplacement = false, 0.1) ++
+      outbounds.sample(withReplacement = false, 0.5)
+
+    output.saveAsTextFile(args("output"))
+
+    sc.close()
+
+    //res.waitForResult().value.foreach(println)
+
+  }
+}
